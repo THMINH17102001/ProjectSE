efbd200afcba10699aa7c69decbc89adb88b69bc
Văn Đình Khôi <63282078+khoivankarik@users.noreply.github.com>
2021-08-24 14:56:57 +0700

    Waiting Room with Firebase

--- app/src/main/java/com/example/projectse/WaitingRoom.java
+++ app/src/main/java/com/example/projectse/WaitingRoom.java
@@ -3,13 +3,22 @@ package com.example.projectse;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.DatabaseErrorHandler;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,66 +27,107 @@ public class WaitingRoom extends AppCompatActivity {
 
     ListView listView;
     Button button;
-    List<String> roomList;
+    List<String> roomsList;
     String playerName="";
     String roomName="";
 
-    //FirebaseDatabase database;
-    //DatabaseReference roomRef;
-    //DatabaseReference roomsRef;
+    FirebaseDatabase database;
+    DatabaseReference roomRef;
+    DatabaseReference roomsRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiting_room);
 
-        //get the player name and assign his roomname as his player name
-        SharedPreferences preferences=getSharedPreferences("PREPS",0);
-        playerName=preferences.getString("playerName","");
-        roomName=playerName;
+        database = FirebaseDatabase.getInstance();
 
-        listView= (ListView)findViewById(R.id.listView);
-        button=findViewById(R.id.button);
+        //get the player name and assign his roomname as his player name
+        SharedPreferences preferences = getSharedPreferences("PREPS", 0);
+        playerName = preferences.getString("playerName", "");
+        roomName = playerName;
+
+        listView = findViewById(R.id.listView);
+        button = findViewById(R.id.button);
 
         //All existing available rooms
-        roomList=new ArrayList<>();
+        roomsList = new ArrayList<>();
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             //Create room and add yourself as player 1
             public void onClick(View view) {
-                button.setText("CREATE ROOM");
+                button.setText("CREATING ROOM");
                 button.setEnabled(false);
-                roomName=playerName;
-                //roomRef=database.getReference("rooms/+roomName+"/player1"");
+                roomName = playerName;
+                roomRef = database.getReference("rooms/" + roomName + "/player1");
                 //addRoomEventListener();
-                //roomRef.setvalue(playerName);
+                roomRef.setValue(playerName);
             }
         });
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                roomName=roomList.get(position);
-                //roomRef=database.getReference("rooms/"+roomName+"/player2;
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //join existing room and add yourself as player2
+                roomName=roomsList.get(position);
+                roomRef=database.getReference("rooms/"+roomName+"/player2");
                 //addRoomEventListener();
-                //roomRef.setvalue(playerName);
+                roomRef.setValue(playerName);
+
+            }
+        });
+        //show if new room is available
+        addRoomsEventListener();
+
+    }
+    private void addRoomEventListener(){
+        roomRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //join the room
+                button.setText("CREATE ROOM");
+                button.setEnabled(true);
+                Intent intent=new Intent(getApplicationContext(),WaitingRoom.class); //fix this to Waiting room 2
+                intent.putExtra("roomName",roomName);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //error
+                button.setText("CREATE ROOM");
+                button.setEnabled(true);
+                Toast.makeText(WaitingRoom.this,"Error!",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void addRoomsEventListener(){
+        roomRef=database.getReference("rooms");
+        roomsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //show list of rooms
+                roomsList.clear();
+                Iterable<DataSnapshot> rooms= snapshot.getChildren();//Update from datasnapshot getchilden
+                for(DataSnapshot snapshot1:rooms)
+                {
+                    roomsList.add(snapshot.getKey());
+                    ArrayAdapter<String> adapter=new ArrayAdapter<>(WaitingRoom.this, android.R.layout.simple_list_item_1,roomsList);
+                    listView.setAdapter(adapter);
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //error - nothing
+
             }
         });
 
-        //Show if new room is available
-        //addRoomEventListener();
     }
-    /*private void addRoomEventListener() {
-        roomRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void OnDataChange(@NonNull DataSnapShot dataSnapShot){
 
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError){
-
-            }
-
-        })
-    }*/
 }
