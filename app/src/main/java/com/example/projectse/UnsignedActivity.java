664dd94773248f8a8243f1d1d67a27beb5cd0230
Nguyễn Thành Nhơn <nhonnguyen3112@gmail.com>
2021-09-09 13:08:36 +0700

    Edit unsign UI

--- app/src/main/java/com/example/projectse/UnsignedActivity.java
+++ app/src/main/java/com/example/projectse/UnsignedActivity.java
@@ -2,6 +2,7 @@ package com.example.projectse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.text.SpannableString;
@@ -15,31 +16,19 @@ import android.view.View;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class UnsignedActivity extends AppCompatActivity {
+public class UnsignedActivity extends Activity {
 
     Button sgleModeSwitchScrBtn, signInSwitchScrBtn, quitBtn;
-    TextView text1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_unsigned);
 
-        //Make a textview bold
-        text1 = findViewById(R.id.multiTextView_UnsignedActivity);
-        String str = "You want to play in multiple player mode ?";
-        SpannableString ss = new SpannableString(str);
-        StyleSpan boldSpan = new StyleSpan(Typeface.BOLD);
-        ForegroundColorSpan colorSpan = new ForegroundColorSpan(Color.rgb(0,0,0));
-        ss.setSpan(boldSpan, 19, 40, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        ss.setSpan(colorSpan, 19, 40,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        text1.setText(ss);
-
         //Buttons
-        sgleModeSwitchScrBtn = findViewById(R.id.singlemode_UnsignedActivity);
-        signInSwitchScrBtn = findViewById(R.id.signin_UnsignedActivity);
-        quitBtn = findViewById(R.id.quit_UnsignedActivity);
+        sgleModeSwitchScrBtn = findViewById(R.id.singlemode);
+        signInSwitchScrBtn = findViewById(R.id.signin);
+        quitBtn = findViewById(R.id.quit);
 
         signInSwitchScrBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
